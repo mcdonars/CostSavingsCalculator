879f17fac86af1a3ea72ba89037f5f125a0bfabf
mcdonars <mcdonars@dukes.jmu.edu>
2019-07-29 18:32:55 -0400

    Added contribution deductions for Yearly Income when calculating federal taxes. Corrected 2nd tax bracket calcuation and included lower limits. Added Notes for future implementations.

--- CostSavingsCalculator.py
+++ CostSavingsCalculator.py
@@ -1,10 +1,15 @@
 # Gather user data
 YearlyIncome = int(input("Please enter your yearly income:" ))
 MonthlyExpenses = int(input("Please enter your monthly expenses: "))
+MonthyContributions = int(input("Please enter your monthly contributions: "))
+
+# Subtract Contributions
+
+# TaxableIncome = YearlyIncome - Contrib
+TaxableIncome = YearlyIncome - (MonthyContributions * 12)
 
 # Subtract Federal Taxes
-
-def Fed_Taxes(YearlyIncome):
+def Fed_Taxes(TaxableIncome):
     # Maximum taxable income for each tax bracket
     MTB1 = 9525
     MTB2 = 38700
@@ -14,15 +19,15 @@ def Fed_Taxes(YearlyIncome):
     TRB2 = 0.12
     TRB3 = 0.22
 
-    if (YearlyIncome > 0) and (YearlyIncome <= MTB1):
-        T1 = (YearlyIncome * TRB1)
+    if (TaxableIncome >= 0) and (TaxableIncome <= MTB1):
+        T1 = (TaxableIncome * TRB1)
         return T1
-    elif (YearlyIncome > MTB1) and (YearlyIncome <= MTB2):
-        T2 = (YearlyIncome - MTB1) * TRB2
+    elif (TaxableIncome >= MTB1) and (TaxableIncome <= MTB2):
+        T2 = (TaxableIncome - MTB1) * TRB2
         return T2 + (MTB1 * TRB1)
-    elif (YearlyIncome > MTB2) and (YearlyIncome <= MTB3):
-        T3 = (YearlyIncome - MTB2) * TRB3
-        return T3 + (MTB2 * MTB2) + (MTB1 * TRB1)
+    elif (TaxableIncome >= MTB2) and (TaxableIncome <= MTB3):
+        T3 = (TaxableIncome - MTB2) * TRB3
+        return T3 + (MTB2 * TRB2) + (MTB1 * TRB1)
 
         # Example calculation with 40,000 income
             # T3 = (40000 - 38700) = 1300 * 0.22
@@ -30,8 +35,8 @@ def Fed_Taxes(YearlyIncome):
             # T1 = (9525 - 0) = 9525 * 0.10
             # TaxesTotal = T1+T2+T3
 
-UserPaidTaxes = Fed_Taxes(YearlyIncome)
-EarningsAT = YearlyIncome - UserPaidTaxes
+UserPaidTaxes = Fed_Taxes(TaxableIncome)
+EarningsAT = TaxableIncome - UserPaidTaxes
 
 print("Your yearly taxes are: ")
 print(UserPaidTaxes)
@@ -43,6 +48,8 @@ print(YearlyExpenses)
 YearlySavings = EarningsAT - YearlyExpenses
 print("Your yearly savings after taxes are: ")
 print(YearlySavings)
+
+
 
 
 # def User_Expense():
